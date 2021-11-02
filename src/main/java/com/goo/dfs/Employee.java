ecdc414bf61f692c3541df01305ee0535e4608e1
SijiaZZZ <91804922+SijiaZZZ@users.noreply.github.com>
2021-11-02 14:43:25 -0700

    [Add][11-2-2021] 1. Add more config 2. E-0690

---
+++ src/main/java/com/goo/dfs/Employee.java
@@ -0,0 +1,10 @@
+package com.goo.dfs;
+
+import java.util.List;
+
+public class Employee {
+  public int id;
+  public int importance;
+  public List<Integer> subordinates;
+
+}
--- src/main/java/com/goo/dfs/LC_0690_Employee_Importance.java
+++ src/main/java/com/goo/dfs/LC_0690_Employee_Importance.java
@@ -1,5 +1,28 @@
 package com.goo.dfs;
 
-public class LC_0690_Employee_Importance {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class LC_0690_Employee_Importance {
+  public int getImportance(List<Employee> employees, int id) {
+    // build a map
+    Map<Integer, Employee> idMap = new HashMap<Integer, Employee>();
+    for (Employee e: employees) {
+      idMap.put(e.id, e);
+    }
+    return getImportanceHelper(idMap, id);
+  }
+
+  private int getImportanceHelper(Map<Integer, Employee> idMap, int id) {
+    Employee e = idMap.get(id);
+    int subImportance = 0;
+    if (e.subordinates.isEmpty()) {
+      return e.importance;
+    }
+    for (int sub: e.subordinates) {
+        subImportance += getImportanceHelper(idMap, sub);
+    }
+    return e.importance + subImportance;
+  }
 }
