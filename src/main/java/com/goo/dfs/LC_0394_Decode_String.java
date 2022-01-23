32fb07647087277d731a64ed8e0045dd82e8a09a
SijiaZZZ <91804922+SijiaZZZ@users.noreply.github.com>
2022-01-22 19:28:06 -0800

    [Add][01-22-2022] linkedList

---
+++ src/main/java/com/goo/dfs/LC_0394_Decode_String.java
@@ -0,0 +1,47 @@
+/**
+ * Created by Sijia on 1/20/22
+ * Time Complexity:
+ * <p>
+ * Space Complexity:
+ * <p>
+ * Hints: 两个stack 一个数字stack 一个 Str stack
+ * <p> 1.
+ * <p> 2.
+ * <p> 3.
+ */
+
+package com.goo.dfs;
+
+import java.util.Stack;
+
+public class LC_0394_Decode_String {
+    public String decodeString(String s) {
+        if (s == null || s.length() == 0) return "";
+        Stack<Integer> numStack = new Stack<>();
+        Stack<StringBuilder> stack = new Stack<>();
+        stack.push(new StringBuilder());
+
+        int val = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                val = val * 10 + (c - '0');
+            } else if (c == '[') {
+                stack.push(new StringBuilder());
+                numStack.push(val);
+                val = 0;
+            } else if (c == ']') {
+                StringBuilder sb = new StringBuilder();
+                StringBuilder temp = stack.pop();
+                int dup = numStack.pop();
+                for (int j = 0; j < dup; j++) {
+                    sb.append(temp);
+                }
+                stack.peek().append(sb);
+            } else {
+                stack.peek().append(c);
+            }
+        }
+        return stack.peek().toString();
+    }
+}
---
+++ src/main/java/com/goo/linkedList/ListNode.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Sijia on 1/20/22
+ * Time Complexity:
+ * <p>
+ * Space Complexity:
+ * <p>
+ * Hints:
+ * <p> 1.
+ * <p> 2.
+ * <p> 3.
+ */
+
+package com.goo.linkedList;
+
+/**
+ * ListNode structure.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
